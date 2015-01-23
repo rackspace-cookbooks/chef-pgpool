3c3c84361417cb7cef798529f5efa87cad40e461
Jens Jorritsma <jens.jorritsma@gmail.com>
2015-01-22 21:00:16 -0600

    Release v0.1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,10 @@
 pgpool Cookbook CHANGELOG
 =========================
 
+0.1.1
+-----
+- Jens Jorritsma - Template fix for pgpool.conf
+
 0.1.0
 -----
 - Jens Jorritsma - Initial release of pgpool
--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'jens.jorritsma@gamil.com'
 license 'All rights reserved'
 description 'Installs/Configures pgpool'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.0'
+version '0.1.1'
