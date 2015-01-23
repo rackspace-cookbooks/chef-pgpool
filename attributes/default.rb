7ef2f6ef80544c387f40e5d4c547a4b0121e5699
Jim Rosser <jarosser06@gmail.com>
2015-01-23 10:33:36 -0600

    added service, make sure directories needed by the service are created

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,16 +1,21 @@
+default['pgpool']['user'] = 'postgres'
+default['pgpool']['group'] = 'postgres'
+
 case node['platform']
 when 'debian', 'ubuntu'
   default['pgpool']['config']['package_name'] = 'pgpool2'
   default['pgpool']['config']['dir'] = '/etc/pgpool2'
+  default['pgpool']['service'] = 'pgpool2'
 when 'centos'
   default['pgpool']['config']['package_name'] = 'postgresql-pgpool-II'
   default['pgpool']['config']['dir'] = '/etc/pgpool-II'
+  default['pgpool']['service'] = 'pgpool'
 end
 
 default['pgpool']['pgconf']['listen_addresses'] = 'localhost'
 default['pgpool']['pgconf']['port'] = 9999
-default['pgpool']['pgconf']['socket_dir'] = '/var/run'
-default['pgpool']['pgconf']['pcp_socket_dir'] = '/var/run'
+default['pgpool']['pgconf']['socket_dir'] = '/var/run/pgpool'
+default['pgpool']['pgconf']['pcp_socket_dir'] = '/var/run/pgpool'
 default['pgpool']['pgconf']['num_init_children'] = 32
 default['pgpool']['pgconf']['enable_pool_hba'] = true
 default['pgpool']['pgconf']['logdir'] = '/var/log/pgpool'
