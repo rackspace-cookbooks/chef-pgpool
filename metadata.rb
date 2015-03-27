0ce2684503851d73a46bf7b0290e8d959c241bce
Igor Gueths <igor.gueths@rackspace.com>
2015-03-27 15:19:35 -0500

    Expose necessary attributes to do streaming replication, bump version
    Previously, any configuration options pertinent to
    using Pgpool with a streaming replication cluster
    were not exposed anywhere, leaving one to fend for
    themselves via role/environment overrides. This
    problem is solved for here by exposing those
    options and others as default attributes, as well
    as relevant documentation on the options that
    should not be left at their defaults.

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,8 @@
-name             'pgpool'
-maintainer       'Jens Jorritsma'
-maintainer_email 'jens.jorritsma@gamil.com'
+name             'keep-pgpool'
+maintainer 'CloudKeep'
+maintainer_email 'keep@rackspace.com'
 license          'Apache 2'
 description      'Installs/Configures pgpool'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          '0.3.0'
+depends 'keep-base'
