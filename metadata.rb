f4feaf1de7566c6e1eb569c4335ce3ba2bf2feb3
Igor Gueths <igor.gueths@rackspace.com>
2015-04-10 17:06:19 -0500

    Reinsert original maintainer info.

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             'pgpool'
 maintainer 'Jens Jorritsma'
-maintainer_email 'jens.jorritsma@rackspace.com'
+maintainer_email 'jens.jorritsma@gamil.com'
 license          'Apache 2'
 description      'Installs/Configures pgpool'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
